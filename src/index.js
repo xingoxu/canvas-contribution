74215781e1f20374e705e467ba6ab86072c219fe
xingoxu <xingoxu@gmail.com>
2017-03-05 23:28:34 +0800

    add stats

--- src/index.js
+++ src/index.js
@@ -16,3 +16,15 @@ contributeCanvas(canvas, testData, {
   },
   text: 'activities',
 });
+var stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
+function animate() {
+
+    stats.begin();
+    // monitored code goes here
+    stats.end();
+    requestAnimationFrame( animate );
+}
+requestAnimationFrame( animate );
